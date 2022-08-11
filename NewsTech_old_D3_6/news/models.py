20228fd611105042f17ecf1d853ddeb2f479b712
AlexBattFrank <102475383+AlexBattFrank@users.noreply.github.com>
2022-08-11 12:01:01 +0300

    Add files via upload

---
+++ NewsTech_old_D3_6/news/models.py
@@ -0,0 +1,93 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
+    ratingAuthor = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.authorUser.username
+
+    def update_rating(self):
+        postRat = self.post_set.aggregate(postRating=Sum('rating'))
+        pRat = 0  # creating intermediate variable
+        pRat += postRat.get('postRating')
+
+        commentRat = self.authorUser.comment_set.aggregate(commentRating=Sum('rating'))
+        cRat = 0  # creating intermediate variable
+        cRat += commentRat.get('commentRating')
+
+        self.ratingAuthor = pRat * 3 + cRat
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    NEWS = 'NW'
+    ARTICLE = 'AR'
+    REGULATION = 'REG'
+    POLLS = 'POL'
+
+    CATEGORY_CHOICES = (
+        (NEWS, 'One String News'),
+        (ARTICLE, 'Articles'),
+        (REGULATION, 'REG'),
+        (POLLS, 'POL'),
+    )
+    categoryType = models.CharField(max_length=4, choices=CATEGORY_CHOICES, default=ARTICLE)
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    postCategory = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+    rating = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.dateCreation}, {self.title}, {self.body}'
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.body[0:16] + '...'
+
+
+class PostCategory(models.Model):
+    postThought = models.ForeignKey(Post, on_delete=models.CASCADE)
+    categoryThought = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.commentPost.author.authorUser.username + '___' + str(self.id)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def __str__(self, commentUser=None):
+        return f'{self.text}, {self.dateCreation}, {self.rating}, {commentUser}'
