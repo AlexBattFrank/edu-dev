5ab9ec706f44dd5ddce7cdb99bde4cbf5704be49
AlexBattFrank <alex.a.bulkin@gmail.com>
2023-01-25 17:18:45 +0300

    Add files via upload

---
+++ HW_4.5.4 (HW-03)/Task1_HW_E4.js
@@ -0,0 +1,12 @@
+
+
+const obj2 = {
+    prop2: "prop2",
+    prop3: "prop3"
+}
+function selfKey(object){
+    console.log(Object.keys(object))
+    console.log(Object.keys(object))
+}
+selfKey(obj2)
+
