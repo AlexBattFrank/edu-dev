f3f9d9e689cc6eca6764bab78285acf4a9cbdab1
AlexBattFrank <alex.a.bulkin@gmail.com>
2023-02-19 17:18:45 +0300

    Delete Test directory

--- Test/myChat/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    # path('rest-auth/', include('rest_auth.urls')),
-    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
-]
--- Test/myChat/websockets/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def my_websocket(request):
-    return render(request, 'websockets/my_websocket.html')
