7c8cd2b140cd7671043941c62dd87665867b6557
AlexBattFrank <alex.a.bulkin@gmail.com>
2023-03-20 12:54:51 +0300

    Update main_1.js

--- CV/js/main_1.js
+++ CV/js/main_1.js
@@ -1,58 +1,49 @@
 // Функциональность для Collapsible Block
-// let i;
-// const coll = document.getElementsByClassName("collapsible");
-//
-// for (i = 0; i < coll.length; i++) {
-//     console.log(i)
-//     const header = coll[i].previousElementSibling;
-//     console.log(header)
-//     header.addEventListener('click', function () {
-//       this.classList.toggle('active');
-//       const content = header.nextElementSibling;
-//       if (content.style.maxHeight){
-//         content.style.maxHeight = null;
-//       } else {
-//         content.style.maxHeight = content.scrollHeight + "px";
-//         }
-//     });
-// }
 
-
-
-const collapsibleTitles = document.querySelectorAll('.collapsible-title');
+const collapsibleTitles = document.querySelectorAll('.data-collapsible-title');
 collapsibleTitles.forEach(collapsibleTitle => {
-  const id = collapsibleTitle.getAttribute('data-collapsible');
-  const contentElement = document.getElementById(id);
-  const collapsibleContents = document.querySelectorAll('.collapsible-content');
+    const id = collapsibleTitle.getAttribute('data-collapsible-title');
+    const contentElement = document.getElementById(id);
+    const collapsibleContents = document.querySelectorAll('.collapsible-content');
 
-  collapsibleContents.forEach(collapsibleContent => {
-    const parentCollapsible = collapsibleContent.closest('.collapsible');
-    parentCollapsible.addEventListener('click', function () {
-      collapsibleContent.classList.toggle('active');
-      if (collapsibleContent.style.maxHeight) {
-        collapsibleContent.style.maxHeight = null;
-      } else {
-        collapsibleContent.style.maxHeight = collapsibleContent.scrollHeight + 'px';
-      }
+    collapsibleContents.forEach(collapsibleContent => {
+        const parentCollapsible = collapsibleContent.closest('.collapsible-title');
+        parentCollapsible.addEventListener('click', function () {
+            collapsibleContent.classList.toggle('active');
+            if (collapsibleContent.style.maxHeight) {
+                collapsibleContent.style.maxHeight = null;
+            } else {
+                collapsibleContent.style.maxHeight = collapsibleContent.scrollHeight + 'px';
+            }
+        });
     });
-  });
 });
 
 // Функциональность для Timeline
-const timelineItems = document.querySelectorAll('.timeline li');
+    const timelineItems = document.querySelectorAll('.timeline li');
 
-timelineItems.forEach(item => {
-  const h3 = item.querySelector('h3');
-  const p = item.querySelector('p');
-  const date = new Date(p.textContent);
-  h3.textContent = date.getFullYear();
-});
+    timelineItems.forEach(item => {
+        const h3 = item.querySelector('h3');
+        const p = item.querySelector('p');
+        const date = new Date(p.textContent);
+        h3.textContent = date.getFullYear();
+    });
 
 // Функциональность для Skillbar
-const skillBars = document.querySelectorAll('.skill-bar-fill');
+    const skillBars = document.querySelectorAll('.skill-bar-fill');
 
-skillBars.forEach(skillBar => {
-  const percent = skillBar.getAttribute('data-percent');
-  skillBar.style.setProperty('--percent', percent);
-  skillBar.classList.add('active');
+    skillBars.forEach(skillBar => {
+        const percent = skillBar.getAttribute('data-percent');
+        skillBar.style.setProperty('--percent', percent);
+        skillBar.classList.add('active');
+    });
+    
+// Функционал для experience
+document.querySelectorAll('.experience li').forEach(item => {
+    const title = item.querySelector('h3');
+    const content = item.querySelectorAll('p');
+
+    title.addEventListener('click', function() {
+        content.forEach(c => c.classList.toggle('hidden'));
+    });
 });
