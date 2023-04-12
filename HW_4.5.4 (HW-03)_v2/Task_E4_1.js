39ccfd15c23982d0afbd60238ad92cabd0a0ab1f
AlexBattFrank <alex.a.bulkin@gmail.com>
2023-04-12 17:57:11 +0300

    Delete HW_4.5.4 (HW-03)_v2 directory

--- HW_4.5.4 (HW-03)_v2/Task_E4_1.js
+++
@@ -1,7 +0,0 @@
-function printProps(obj) {
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            console.log(`${key}: ${obj[key]}`);
-        }
-    }
-}
--- HW_4.5.4 (HW-03)_v2/Task_E4_3.js
+++
@@ -1,5 +0,0 @@
-
-const createEmptyObject = () => Object.create(null);
-
-// 4 Checking the result
-console.log(createEmptyObject())
